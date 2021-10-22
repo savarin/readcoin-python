9e9d23bd638af6af32442da727a32d34231ff233
Ezzeri Esa <ezzeri.esa@gmail.com>
2021-10-22 13:40:28 -0700

    validate blockchain from other nodes

--- src/blocks.py
+++ src/blocks.py
@@ -3,9 +3,10 @@ import hashlib
 
 
 VERSION: int = 0
+HEADER_SIZE: int = 69
 
 
-def init_block_header(previous_hash: bytes, timestamp: int, nonce: int) -> bytes:
+def init_header(previous_hash: bytes, timestamp: int, nonce: int) -> bytes:
     """ """
     return (
         VERSION.to_bytes(1, byteorder="big")
@@ -26,15 +27,13 @@ def init_transactions(node_port: int):
     return transaction_counter, transactions
 
 
-def init_block(
-    block_header: bytes, transaction_counter: int, transactions: bytes
-) -> bytes:
+def init_block(header: bytes, transaction_counter: int, transactions: bytes) -> bytes:
     """ """
-    block_size = 1 + 41 + 1 + len(transactions)
+    block_size = 1 + HEADER_SIZE + 1 + len(transactions)
 
     return (
         block_size.to_bytes(1, byteorder="big")
-        + block_header
+        + header
         + transaction_counter.to_bytes(1, byteorder="big")
         + transactions
     )
@@ -46,12 +45,12 @@ def init_genesis_block() -> Tuple[bytes, bytes]:
     timestamp = 1634700000
     nonce = 70822
 
-    block_header = init_block_header(previous_hash, timestamp, nonce)
-    guess = hashlib.sha256(hashlib.sha256(block_header).digest())
+    header = init_header(previous_hash, timestamp, nonce)
+    guess = hashlib.sha256(hashlib.sha256(header).digest())
     assert guess.hexdigest()[:4] == "0000"
 
     transaction_counter, transactions = init_transactions(7000)
-    block = init_block(block_header, transaction_counter, transactions)
+    block = init_block(header, transaction_counter, transactions)
 
     return guess.digest(), block
 
@@ -64,19 +63,49 @@ def run_proof_of_work(
 ) -> Tuple[bool, int, Optional[bytes], Optional[bytes]]:
     """Find nonce that makes the first 4 bytes of twice-hashed header all zeroes. Maximum number of
     iterations can be specified."""
-    counter = 0
+    iteration_counter = 0
 
     while True:
-        block_header = init_block_header(previous_hash, timestamp, nonce)
-        guess = hashlib.sha256(hashlib.sha256(block_header).digest())
+        if iterations is not None and iteration_counter == iterations:
+            return False, nonce, None, None
+
+        header = init_header(previous_hash, timestamp, nonce)
+        guess = hashlib.sha256(hashlib.sha256(header).digest())
 
         if guess.hexdigest()[:4] == "0000":
             break
 
-        if iterations is not None and counter == iterations:
-            return False, nonce, None, None
-
         nonce += 1
-        counter += 1
+        iteration_counter += 1
 
-    return True, nonce, guess.digest(), block_header
+    return True, nonce, guess.digest(), header
+
+
+def validate_blockchain(blockchain: bytes) -> Tuple[bool, int, Optional[bytes]]:
+    """ """
+    byte_index = 0
+    blockchain_counter = 0
+
+    blockchain_size = len(blockchain)
+    previous_hash = (0).to_bytes(32, byteorder="big")
+
+    while True:
+        block_size = blockchain[byte_index]
+        header = blockchain[byte_index + 1 : byte_index + 1 + HEADER_SIZE]
+
+        if header[1:33] != previous_hash:
+            return False, blockchain_counter, None
+
+        guess = hashlib.sha256(hashlib.sha256(header).digest())
+
+        if guess.hexdigest()[:4] != "0000":
+            return False, blockchain_counter, None
+
+        byte_index += block_size
+        blockchain_counter += 1
+        previous_hash = guess.digest()
+
+        if byte_index == blockchain_size:
+            break
+
+    return True, blockchain_counter, previous_hash
