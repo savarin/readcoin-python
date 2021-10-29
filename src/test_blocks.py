7483fa0a5c0622a288182d07c85b5aa60f500586
Ezzeri Esa <ezzeri.esa@gmail.com>
2021-10-28 22:32:00 -0700

    add test for validating transactions

--- src/test_blocks.py
+++ src/test_blocks.py
@@ -11,6 +11,7 @@ def test_proof_of_work():
     is_new_block, nonce, current_hash, header = blocks.run_proof_of_work(
         previous_hash, timestamp, 0, 1000
     )
+
     assert not is_new_block
     assert nonce == 1000
     assert current_hash is None
@@ -19,6 +20,7 @@ def test_proof_of_work():
     is_new_block, nonce, current_hash, header = blocks.run_proof_of_work(
         previous_hash, timestamp, 70000, 1000
     )
+
     assert is_new_block
     assert nonce == 70822
     assert (
@@ -36,6 +38,7 @@ def test_proof_of_work():
     is_new_block, nonce, current_hash, header = blocks.run_proof_of_work(
         previous_hash, timestamp, 0, 1000
     )
+
     assert not is_new_block
     assert nonce == 1000
     assert current_hash is None
@@ -44,6 +47,7 @@ def test_proof_of_work():
     is_new_block, nonce, current_hash, header = blocks.run_proof_of_work(
         previous_hash, timestamp, 58000, 1000
     )
+
     assert is_new_block
     assert nonce == 58768
     assert (
@@ -59,12 +63,14 @@ def test_proof_of_work():
 def test_validate_blockchain():
     """ """
     _, blockchain = blocks.init_genesis_block()
-    is_new_block, blockchain_counter, current_hash, _ = blocks.validate_blockchain(
+    transaction_counter, transactions = blocks.init_transactions(7000)
+
+    is_new_block, block_counter, current_hash, _ = blocks.validate_blockchain(
         blockchain
     )
 
     assert is_new_block
-    assert blockchain_counter == 1
+    assert block_counter == 1
     assert (
         binascii.hexlify(current_hash)
         == b"00001ff58495c3dc2a1aaa69ebca4e9b3e05e63e5a319fb73bcdccbcdbba1e72"
@@ -72,18 +78,17 @@ def test_validate_blockchain():
 
     previous_hash = current_hash
     timestamp = 1634700600
-    transaction_counter, transactions = blocks.init_transactions(7000)
 
     _, _, _, header = blocks.run_proof_of_work(previous_hash, timestamp, 58000, 1000)
     block = blocks.init_block(header, transaction_counter, transactions)
     blockchain += block
 
-    is_new_block, blockchain_counter, current_hash, block = blocks.validate_blockchain(
+    is_new_block, block_counter, current_hash, block = blocks.validate_blockchain(
         blockchain
     )
 
     assert is_new_block
-    assert blockchain_counter == 2
+    assert block_counter == 2
     assert (
         binascii.hexlify(current_hash)
         == b"000071e6ff5b358e57339b42d45b20acc0f112c218fa435b3ffa8f239b777347"
@@ -91,10 +96,64 @@ def test_validate_blockchain():
 
     header = (0).to_bytes(32, byteorder="big")
     block = blocks.init_block(header, transaction_counter, transactions)
-    is_new_block, blockchain_counter, current_hash, _ = blocks.validate_blockchain(
-        blockchain + block
+    blockchain += block
+
+    is_new_block, block_counter, current_hash, _ = blocks.validate_blockchain(
+        blockchain
     )
 
     assert not is_new_block
-    assert blockchain_counter == 3
+    assert block_counter == 3
     assert current_hash is None
+
+
+def test_validate_transactions():
+    """ """
+    _, blockchain = blocks.init_genesis_block()
+    transaction_counter, transactions = blocks.init_transactions(7000)
+
+    _, _, previous_hash, _ = blocks.validate_blockchain(blockchain)
+    _, _, _, header = blocks.run_proof_of_work(previous_hash, 1634700600, 58000, 1000)
+    block = blocks.init_block(header, transaction_counter + 1, transactions)
+
+    reference_hash = previous_hash
+    sender = (7000).to_bytes(2, byteorder="big")
+    receiver = (8000).to_bytes(2, byteorder="big")
+    transfer = reference_hash + sender + receiver
+
+    block += transfer
+    block = (block[0] + len(transfer)).to_bytes(1, byteorder="big") + block[1:]
+    blockchain += block
+
+    current_transaction_counter = int.from_bytes(
+        block[1 + blocks.HEADER_SIZE : 1 + blocks.HEADER_SIZE + 1], byteorder="big"
+    )
+    current_transactions = block[1 + blocks.HEADER_SIZE + 1 :]
+
+    is_valid_transaction = blocks.validate_transactions(
+        blockchain, current_transactions, current_transaction_counter
+    )
+
+    assert is_valid_transaction
+
+    _, _, previous_hash, _ = blocks.validate_blockchain(blockchain)
+    _, _, _, header = blocks.run_proof_of_work(previous_hash, 1634701200, 3800, 1000)
+    block = blocks.init_block(header, transaction_counter + 1, transactions)
+
+    block += transfer
+    block = (block[0] + len(transfer)).to_bytes(1, byteorder="big") + block[1:]
+    blockchain += block
+
+    current_transaction_counter = (
+        int.from_bytes(
+            block[1 + blocks.HEADER_SIZE : 1 + blocks.HEADER_SIZE + 1], byteorder="big"
+        )
+        + 1
+    )
+    current_transactions = block[1 + blocks.HEADER_SIZE + 1 :]
+
+    is_valid_transaction = blocks.validate_transactions(
+        blockchain, current_transactions, current_transaction_counter
+    )
+
+    assert not is_valid_transaction
