ad3582290e85b5780a0c1884f8b7960f931ea896
Ezzeri Esa <ezzeri.esa@gmail.com>
2021-11-23 15:00:54 -0800

    v4 collect into balances

---
+++ src/balances.py
@@ -0,0 +1,87 @@
+from typing import DefaultDict, List, Optional, Tuple
+import collections
+import dataclasses
+import hashlib
+
+import blocks
+import transactions as transacts
+
+
+Account = DefaultDict[int, List[blocks.Hash]]
+
+
+@dataclasses.dataclass
+class Balance:
+    """ """
+
+    latest_hash: blocks.Hash
+    account: Account
+
+
+def update_account_with_block(
+    account: Account, blockchain: blocks.Blockchain, block_hash: blocks.Hash
+) -> Account:
+    """ """
+    block = blockchain.blocks[block_hash]
+
+    for transaction in block.transactions:
+        if transaction.sender != 0:
+            account[transaction.sender].remove(transaction.reference_hash)
+
+        reference_hash = hashlib.sha256(transaction.encode()).digest()
+        account[transaction.receiver].append(reference_hash)
+
+    return account
+
+
+def init_balance(blockchain: blocks.Blockchain) -> Balance:
+    """ """
+    account: Account = collections.defaultdict(list)
+
+    for block_hash in blockchain.chain:
+        account = update_account_with_block(account, blockchain, block_hash)
+
+    return Balance(latest_hash=block_hash, account=account)
+
+
+def update_balance_with_blockchain(
+    balance: Balance, blockchain: blocks.Blockchain
+) -> Tuple[bool, Optional[Balance]]:
+    """ """
+    if balance.latest_hash not in blockchain.chain:
+        return False, None
+
+    hash_index = blockchain.chain.index(balance.latest_hash)
+
+    for block_hash in blockchain.chain[hash_index + 1 :]:
+        balance.account = update_account_with_block(
+            balance.account, blockchain, block_hash
+        )
+
+    return True, balance
+
+
+def init_transfer(
+    balance: Balance, sender: int, receiver: int
+) -> Tuple[Optional[Balance], Optional[transacts.Transaction]]:
+    """ """
+    if sender not in balance.account or len(balance.account[sender]) == 0:
+        return None, None
+
+    reference_hash = balance.account[sender].pop(0)
+    transaction = transacts.Transaction(
+        reference_hash=reference_hash, sender=sender, receiver=receiver
+    )
+
+    return balance, transaction
+
+
+def validate_transfer(balance: Balance, transaction: transacts.Transaction) -> bool:
+    """ """
+    if (
+        transaction.sender not in balance.account
+        or len(balance.account[transaction.sender]) == 0
+    ):
+        return False
+
+    return transaction.reference_hash in balance.account[transaction.sender]
