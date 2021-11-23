55dfd7c30824cd65a011b33b53070a9f7991c0fd
Ezzeri Esa <ezzeri.esa@gmail.com>
2021-11-22 16:54:50 -0800

    simplify tree

--- src/tree.py
+++ src/tree.py
@@ -37,10 +37,9 @@ def init_merkle_tree(transaction_hashes: List[Hash]) -> Optional[Tree]:
     right = init_merkle_tree(transaction_hashes[divider_index:])
 
     assert left is not None and right is not None
-    left_hash = left.tree_hash if left is not None else b""
-    right_hash = right.tree_hash if right is not None else b""
+    left_hash = left.tree_hash
+    right_hash = right.tree_hash
 
-    assert left_hash is not None or right_hash is not None
     tree_hash = hashlib.sha256(left_hash + right_hash).digest()
 
     return Tree(tree_hash=tree_hash, left=left, right=right)
@@ -63,22 +62,14 @@ def find_merkle_path(merkle_tree: Tree, transaction_hash: Hash) -> Optional[List
         right_tree = current_tree.right
 
         if left_tree is not None:
-            left_path = []
-
-            if right_tree is not None:
-                left_path += [right_tree.tree_hash]
-
-            # left_path += [left_tree.tree_hash]
-            queue.append((path + left_path, left_tree))
+            assert right_tree is not None
+            left_path = path + [right_tree.tree_hash]
+            queue.append((path + [right_tree.tree_hash], left_tree))
 
         if right_tree is not None:
-            right_path = []
-
-            if left_tree is not None:
-                right_path += [left_tree.tree_hash]
-
-            # right_path += [right_tree.tree_hash]
-            queue.append((path + right_path, right_tree))
+            assert left_tree is not None
+            right_path = path + [left_tree.tree_hash]
+            queue.append((path + [left_tree.tree_hash], right_tree))
 
     return None
 
@@ -106,14 +97,14 @@ def validate_merkle_path(path: Optional[List[bytes]]):
             left_hash = hashlib.sha256(current_hash + child_hash).digest()
             right_hash = hashlib.sha256(child_hash + current_hash).digest()
 
-            current_left = Tree(tree_hash=left_hash, left=None, right=None)
-            current_right = Tree(tree_hash=right_hash, left=None, right=None)
+            left = Tree(tree_hash=left_hash, left=None, right=None)
+            right = Tree(tree_hash=right_hash, left=None, right=None)
 
-            current_tree.left = current_left
-            current_tree.right = current_right
+            current_tree.left = left
+            current_tree.right = right
 
-            subtrees_out += [(current_left.tree_hash, current_left)]
-            subtrees_out += [(current_right.tree_hash, current_right)]
+            subtrees_out += [(left_hash, left)]
+            subtrees_out += [(right_hash, right)]
 
         subtrees_in = subtrees_out.copy()
         subtrees_out = []
