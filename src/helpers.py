a20b9abdb756e6a700c985c7762520df86da911c
Ezzeri Esa <ezzeri.esa@gmail.com>
2021-12-13 20:57:51 -0800

    update readme

--- src/helpers.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-
-def bind_socket(ip_address: str, port: int) -> socket.socket:
-    """ """
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((ip_address, port))
-    return sock
